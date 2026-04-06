00f022aef72fd0770866bce26ea7277859885bfb
Mahima Patel <mahima.patel@perficient.com>
2026-04-06 19:52:09 +0530

    Added bypass dam url

--- examples/kit-nextjs-skate-park/next.config.ts
+++ examples/kit-nextjs-skate-park/next.config.ts
@@ -26,6 +26,12 @@ const nextConfig: NextConfig = {
         hostname: 'xmc-*.**',
         port: '',
       },
+      {
+        protocol: 'https',
+        hostname: 'prft-demo.sitecoresandbox.cloud', 
+        port: '',
+        pathname: '/**',
+      },
     ],
     // Disable image optimization in development to avoid upstream timeouts
     unoptimized: process.env.NODE_ENV === 'development',
